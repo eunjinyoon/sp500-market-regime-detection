7b7e99ff382b1517efff453876bf4d3a9ec03d1f
eunjinyoon <eunjinyoon.ej@gmail.com>
2026-08-03 20:09:41 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import yfinance as yf
-
-print("pandas:", pd.__version__)
-print("numpy:", np.__version__)
-print("yfinance:", yf.__version__)
-
